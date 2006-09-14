792315b8a470eac1752b57f70f52566cd711cbae
darkdragon <darkdragon@cbb77c4c-441d-0410-a7c6-eed6ac826a58>
2006-09-14 20:20:34 +0000

    Forget to delete old sites and add the new.

---
+++ trunk/_base/sites/0003-test.php
@@ -0,0 +1,13 @@
+<?php 
+$SSettings = array();
+$SBlocks = array();
+$SSettings['template'] = 'standard/standard.tpl'; 
+$SSettings['title'] = 'test'; 
+$SSettings['path'] = '/test'; 
+$SBlocks['header'] = '%cblocks%/header/'; 
+$SBlocks['topmenu'] = '%cblocks%/topmenu/'; 
+$SBlocks['leftblock'] = '%cblocks%/leftblock/'; 
+$SBlocks['rightblock'] = '%cblocks%/rightblock/'; 
+$SBlocks['footer'] = '%cblocks%/footer/'; 
+$SBlocks['content'] = '%cblocks%/content/'; 
+?>
---
+++ trunk/_base/sites/0004-test2.php
@@ -0,0 +1,13 @@
+<?php 
+$SSettings = array();
+$SBlocks = array();
+$SSettings['template'] = 'standard/standard.tpl'; 
+$SSettings['title'] = 'test2'; 
+$SSettings['path'] = '/test2'; 
+$SBlocks['header'] = '%cblocks%/header/'; 
+$SBlocks['topmenu'] = '%cblocks%/topmenu/'; 
+$SBlocks['leftblock'] = '%cblocks%/leftblock/'; 
+$SBlocks['rightblock'] = '%cblocks%/rightblock/'; 
+$SBlocks['footer'] = '%cblocks%/footer/'; 
+$SBlocks['content'] = '%cblocks%/content/'; 
+?>
